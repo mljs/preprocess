5a13bdb7b449cbf0be08fdc484e7c0551f682aa6
jobo322 <jose.bolanos@correounivalle.edu.co>
2017-03-14 13:14:23 -0500

    scaling methods added

---
+++ src/pre/scaling.js
@@ -0,0 +1,47 @@
+'use strict';
+
+
+const Matrix = require('ml-matrix');
+const Stat = require('ml-stat');
+
+function pareto(dataSet, mean, std) {
+    var means = mean || Stat.matrix.mean(dataSet);
+    var std = std || Stat.matrix.standardDeviation(dataSet, means, true);
+    var result = Matrix.checkMatrix(dataSet).subRowVector(means)
+    return result.divRowVector(Math.sqrt(std));
+}
+
+function autoScaling(dataSet, mean, std) {
+    var means = mean || Stat.matrix.mean(dataSet);
+    var std = std || Stat.matrix.standardDeviation(dataSet, means, true);
+    var result = Matrix.checkMatrix(dataSet).subRowVector(means)
+    return result.divRowVector(std);
+}
+
+function vastScaling(dataSet, mean, std) {
+    var means = mean || Stat.matrix.mean(dataSet);
+    var std = std || Stat.matrix.standardDeviation(dataSet, means, true);
+    var result = autoScaling(dataSet, means, std);
+    result.mulColumnVector(means)
+    return result.divColumnVector(std);
+}
+
+function centering(dataSet, mean) {
+    var means = mean || Stat.matrix.mean(dataSet);
+    var std = std || Stat.matrix.standardDeviation(dataSet, means, true);
+    return Matrix.checkMatrix(dataSet).subRowVector(means);
+}
+
+function levelScaling(dataSet, mean) {
+    var means = mean || Stat.matrix.mean(dataSet);
+    var result = Matrix.checkMatrix(dataSet).subRowVector(means)
+    return result.divColumnVector(means);
+}
+
+module.exports = {
+    levelScaling: levelScaling,
+    centering: centering,
+    vastScaling: vastScaling,
+    autoScaling: autoScaling,
+    pareto: pareto
+};
