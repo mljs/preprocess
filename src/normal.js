8f9202f50c8157d80aa0d0de772aeb57a0e52fb2
maasencioh <maasencioh@gmail.com>
2017-04-28 20:15:56 -0500

    refactor(normal): update to em6

--- src/normal.js
+++ src/normal.js
@@ -4,45 +4,46 @@ const stat = require('ml-stat');
 const array = stat.array;
 const matrix = stat.matrix;
 
-module.exports = function normal(x) {
-  var X = x.slice();
+/**
+ *
+ * @param {Array<number>|Array<Array<number>>} x
+ * @return {*}
+ */
+function normal(x) {
+    let X = x.slice();
 
-  // is not an array
-  if (X.length === undefined) {
-    return undefined;
-  }
-
-  // is a vector
-  else if (X[0].length === undefined) {
-    var m = array.mean(X);
-    var s = array.standardDeviation(X, true);
-    if (s === 0) {
-      for (var i = 0; i < X.length; i++) {
-        X[i] = (X[i] - m);
-      }
-    }
-    else {
-      for (var i = 0; i < X.length; i++) {
-        X[i] = (X[i] - m) / s;
-      }
-    }
-    return X;
-  }
-
-  // is a matrix
-  else if (X[0][0].length === undefined) {
-    var m = matrix.mean(X, 0);
-    var s = matrix.standardDeviation(X, m, true);
-    for (var i = 0; i < X.length; i++) {
-      for (var j = 0; j < X[i].length; j++) {
-        if (s[i] === 0) {
-          X[i][j] = (X[i][j] - m[i]);
+    if (!X.length) {
+        // is not an array
+        return undefined;
+    } else if (X[0].length === undefined) {
+        // is a vector
+        let m = array.mean(X);
+        let s = array.standardDeviation(X, true);
+        if (s === 0) {
+            for (let i = 0; i < X.length; i++) {
+                X[i] = (X[i] - m);
+            }
+        } else {
+            for (let i = 0; i < X.length; i++) {
+                X[i] = (X[i] - m) / s;
+            }
         }
-        else {
-          X[i][j] = (X[i][j] - m[i]) / s[i];
+    } else if (X[0][0].length === undefined) {
+        // is a matrix
+        let m = matrix.mean(X, 0);
+        let s = matrix.standardDeviation(X, m, true);
+        for (let i = 0; i < X.length; i++) {
+            for (let j = 0; j < X[i].length; j++) {
+                if (s[i] === 0) {
+                    X[i][j] = (X[i][j] - m[i]);
+                } else {
+                    X[i][j] = (X[i][j] - m[i]) / s[i];
+                }
+            }
         }
-      }
     }
+
     return X;
-  }
 }
+
+module.exports = normal;
