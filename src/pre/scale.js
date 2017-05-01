421a26afd57aa51b28efed92f60547cbb97b78a9
jobo322 <jose.bolanos@correounivalle.edu.co>
2017-05-01 17:29:39 +0200

    added documentation

--- src/pre/scale.js
+++ src/pre/scale.js
@@ -1,25 +1,56 @@
 'use strict';
-const utils = require('./utils')
-
+const utils = require('./utils');
+/**
+ * return the pareto scaled data
+ * @param {Array<number>|Array<Array<number>>} dataSet
+ * @param {Array} means
+ * @param {Array} std
+ * @return {Matrix}
+ * @link  https://bmcgenomics.biomedcentral.com/articles/10.1186/1471-2164-7-142
+ */
 exports.pareto = function pareto(dataSet, means, std) {
     [dataSet, means, std] = utils.ajustData(dataSet, means, std);
-    return dataSet.subRowVector(means).divRowVector(std.map(a => {return Math.sqrt(a)}));
-}
-
+    return dataSet.subRowVector(means).divRowVector(std.map(a => {
+        return Math.sqrt(a);
+    }));
+};
+/**
+ * return the auto scaled data
+ * @param {Array<number>|Array<Array<number>>} dataSet
+ * @param {Array} means
+ * @param {Array} std
+ * @return {Matrix}
+ * @link  https://bmcgenomics.biomedcentral.com/articles/10.1186/1471-2164-7-142
+ */
 exports.auto = function auto(dataSet, means, std) {
     [dataSet, means, std] = utils.ajustData(dataSet, means, std);
     return dataSet.subRowVector(means).divRowVector(std);
-}
-
+};
+/**
+ * return the vast scaled data
+ * @param {Array<number>|Array<Array<number>>} dataSet
+ * @param {Array} means
+ * @param {Array} std
+ * @return {Matrix}
+ * @link  https://bmcgenomics.biomedcentral.com/articles/10.1186/1471-2164-7-142
+ */
 exports.vast = function vast(dataSet, means, std) {
     [dataSet, means, std] = utils.ajustData(dataSet, means, std);
     var result = dataSet.subRowVector(means);
     result.mulRowVector(means);
-    return result.divRowVector(std.map(a => {return (a*a)}));
-}
-
+    return result.divRowVector(std.map(a => {
+        return (a * a);
+    }));
+};
+/**
+ * return the level scaled data
+ * @param {Array<number>|Array<Array<number>>} dataSet
+ * @param {Array} means
+ * @return {Matrix}
+ * @link  https://bmcgenomics.biomedcentral.com/articles/10.1186/1471-2164-7-142
+ */
 exports.level = function level(dataSet, means) {
     [dataSet, means] = utils.ajustData(dataSet, means, 0);
     return dataSet.subRowVector(means).divRowVector(means);
-}
+};
 
--- src/pre/utils.js
+++ src/pre/utils.js
@@ -6,11 +6,13 @@ const Matrix = require('ml-matrix');
 exports.ajustData = function ajustData(dataSet, means, std) {
     if (Array.isArray(dataSet[1])) {
         dataSet = Matrix.checkMatrix(dataSet);
-    } else if(Array.isArray(dataSet)) {
-        dataSet = new Matrix(dataSet.map(a => {return [a]}));
+    } else if (Array.isArray(dataSet)) {
+        dataSet = new Matrix(dataSet.map(a => {
+            return [a];
+        }));
     }
     means = means || Stat.matrix.mean(dataSet);
     std = std || Stat.matrix.standardDeviation(dataSet, means, true);
     return [dataSet, means, std];
-}
+};
 
