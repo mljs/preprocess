a3710058ef19744d2a263a9eaadbd71b1843698e
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-06-14 10:06:33 +0200

    chore: update adjustData api

--- src/preprocess/center.js
+++ src/preprocess/center.js
@@ -8,6 +8,6 @@ import {adjustData} from './utils';
  * @link https://bmcgenomics.biomedcentral.com/articles/10.1186/1471-2164-7-142
  */
 export function center(dataSet, means) {
-    [dataSet, means] = adjustData(dataSet, means, 0);
+    [dataSet, means] = adjustData(dataSet, {means, std: [0]});
     return dataSet.subRowVector(means);
 }
